da150a5e8b11d9e9d9cd16992d923161770a8d6a
Guilherme Francisco Alves Ribeiro <101433358+Guilherme549@users.noreply.github.com>
2024-04-07 15:15:24 -0300

    Adicionado estrutura do projeto, Rotas e Css

--- cost/src/App.js
+++ cost/src/App.js
@@ -1,7 +1,33 @@
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import Home from './components/pages/Home';
+import Company from './components/pages/Company';
+import Contact from './components/pages/Contact';
+import NewProject from './components/pages/NewProject';
+import Container from './components/layout/Container';
+
+
+
+
 function App() {
   return (
     <div className="App">
-      <p>Cost</p>
+      <Router>
+        <ul>
+          <Link to="/">Home</Link>
+          <Link to="/company">Empresa</Link>
+          <Link to="/contact">Contato</Link>
+          <Link to="/newproject">Novo Projeto</Link>
+        </ul>
+        <Container customClass="min-height">
+          <Routes>
+            <Route path='/' element={<Home></Home>}></Route>
+            <Route path='/company' element={<Company></Company>}></Route>
+            <Route path='/contact' element={<Contact></Contact>}></Route>
+            <Route path='/newproject' element={<NewProject></NewProject>}></Route>         
+        </Routes>
+        </Container>
+        <p>Footer</p>
+      </Router>
     </div>
   );
 }
