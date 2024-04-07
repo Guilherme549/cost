1ad425b80c8760c0bd1a9af1436c8e89775ffbcb
Guilherme Francisco Alves Ribeiro <101433358+Guilherme549@users.noreply.github.com>
2024-04-07 17:52:13 -0300

    Criando a pagina de formulario do projeto

--- cost/src/components/pages/NewProject.js
+++ cost/src/components/pages/NewProject.js
@@ -1,8 +1,13 @@
+import ProjectForm from ".././project/ProjectForm"
+import styles from "./NewProject.module.css"
+
 function NewProject(){
     return(
-        <>
-            <h1>NewProject</h1>
-        </>
+        <div className={styles.newproject_container}>
+            <h1>Criar projeto</h1>
+            <p>Crie seu projeto para depois adcionar os serviços</p>
+            <ProjectForm></ProjectForm>
+        </div>
     )
 }
 
